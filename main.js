d3815d64c737c0946206e4e0a31c504ed1c64dc5
Tobias Hollander <tobiashollander@Tobiass-MacBook-Air.local>
2026-02-27 15:09:47 +0900

    added a function to output a random affirmation messages from the affirmations array

---
+++ main.js
@@ -0,0 +1,22 @@
+const affirmations = ["Just because it’s taking time doesn't mean it’s not happening",
+                        "Your life unfolds in proportion to your courage.",
+                        "If you don’t like something, just take away its only power. Your attention.",
+                        "If I keep showing up, then life will reward me.",
+                        "When you visualize, then you materialize. If you’ve been there in the mind, you’ll go there in the body.",
+                        "Clarity usually comes after you take the step. Waiting to feel ready is often what keeps you stuck.",
+                        "To live your greatest life, you must first become a leader within yourself. Take charge of your life, begin attracting and manifesting all that you desire in life.",
+                        "You’re allowed to change your mind. Growth isn’t betrayal, it’s information.",
+                        "Life is only as magical and beautiful as the lens you see it through so, please remember, nothing will improve until your mind gives it a chance to.",
+                        "The entire point of life is to take chances on dreams that seem crazy to most, but feel like destiny to you."]
+
+const button = document.getElementById('button');
+
+// Randomize Affirmation Message function
+const randomAffirmation = arry => {
+    let arryLength = arry.length;
+    let randomIndex = Math.floor(Math.random() * arryLength);
+    return arry[randomIndex]
+};
+// Changing Message function
+
+// onclick Event function
